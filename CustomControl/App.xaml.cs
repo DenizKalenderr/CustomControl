428453727d1542cf83c6e3328985e2adc6642c21
Deniz Kalender <denzkalendr19@gmail.com>
2024-03-11 15:11:51 +0300

    Proje dosyası ekle.

---
+++ CustomControl/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CustomControl
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ CustomControl/FanCC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomControl
+{
+    public class FanCC : Control
+    {
+
+
+        public bool FanOn
+        {
+            get { return (bool)GetValue(FanOnProperty); }
+            set { SetValue(FanOnProperty, value); }
+        }
+
+        public static readonly DependencyProperty FanOnProperty = DependencyProperty.Register("FanOn", typeof(bool), typeof(FanCC), new PropertyMetadata(false));
+
+
+        static FanCC()
+        {
+       
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FanCC), new FrameworkPropertyMetadata(typeof(FanCC)));
+        }
+    }
+}
